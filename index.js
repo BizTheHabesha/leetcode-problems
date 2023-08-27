21011d129561dc1123af74fa0dc7c11fe0b747c3
Biz <github@bisrat.dev>
2023-08-27 08:04:47 -0600

    add showcase for suite

---
+++ index.js
@@ -0,0 +1,31 @@
+import TestingSuite from "./suite.js";
+
+const suite = new TestingSuite(
+	(input) => (input === "fail" ? "no fail" : input),
+	[
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "hello", output: "hello" },
+		{ input: "world", output: "world" },
+		{ input: "hello world", output: "hello world" },
+		{ input: "fail", output: "no fail" },
+	]
+);
+
+suite.run();
