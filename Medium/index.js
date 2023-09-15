0589dfd365a1eb423d8ba7f002a3c31d264bb2e3
Biz <github@bisrat.dev>
2023-09-15 10:46:02 -0600

    add medium imp

---
+++ Medium/index.js
@@ -0,0 +1,13 @@
+import { test122 } from "./122. Best Time to Buy and Sell Stock II/index.js";
+import { test767 } from "./767. Reorganize String/index.js";
+
+test122.debugName();
+test122.run();
+
+test767.debugName();
+test767.run();
+
+export const tests = {
+	122: test122,
+	767: test767,
+};
