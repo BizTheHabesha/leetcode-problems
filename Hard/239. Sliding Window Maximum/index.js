37e44e2cf091dc433ba7171f6c82ebb66109c42a
Biz <github@bisrat.dev>
2023-09-15 10:24:10 -0600

    add 239

--- Hard/239. Sliding Window Maximum/index.js
+++ Hard/239. Sliding Window Maximum/index.js
@@ -1,12 +1,38 @@
-import TestingSuite from "../../suite";
-// You are given an array of integers nums, there is a sliding window of size k which
-// is moving from the very left of the array to the very right.
-// You can only see the k numbers in the window.
-// Each time the sliding window moves right by one position.
-// Return the max sliding window.
+import TestingSuite from "../../suite.js";
+
 /**
+ * You are given an array of integers nums, there is a sliding window of size k which
+ * is moving from the very left of the array to the very right.
+ * You can only see the k numbers in the window.
+ * Each time the sliding window moves right by one position.
+ * Return the max sliding window.
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
-var maxSlidingWindow = function (nums, k) {};
+var maxSlidingWindow = function (nums, k) {
+	let max = [];
+	for (let i = 0; i < nums.length - k + 1; i++) {
+		max[i] = Math.max(...nums.slice(i, i + k));
+	}
+	return max;
+};
+
+const test = new TestingSuite(maxSlidingWindow, [
+	{
+		input: [[1, 3, -1, -3, 5, 3, 6, 7], 3],
+		output: [3, 3, 5, 5, 6, 7],
+	},
+	{
+		input: [[1], 1],
+		output: [1],
+	},
+	{
+		input: [[-7, -8, 7, 5, 7, 1, 6, 0], 50000],
+		output: [7],
+	},
+]);
+
+test.setName("maxSlidingWindow");
+
+export { test as test239, maxSlidingWindow };
---
+++ Hard/index.js
@@ -0,0 +1,8 @@
+import { test239 } from "./239. Sliding Window Maximum/index.js";
+
+test239.debugName();
+test239.run();
+
+export const tests = {
+	239: test239,
+};
